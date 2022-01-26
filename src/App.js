55b95e65e59d4423e66c97a56136e2520a135ea1
dipanc1 <dipanchhabra@gmail.com>
2022-01-26 18:46:07 +0530

    added apollo client and graphql

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import './App.css';
 
 function App() {
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'http://localhost:4000/graphql',
+  });
+
   return (
-    <div className="App">
-      Hello World!!
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        Hello World!!
+      </div>
+    </ApolloProvider>
   );
 }
 
